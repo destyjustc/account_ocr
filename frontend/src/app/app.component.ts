28497ca6fda1f734e2667c25a420947be1e000ed
destyjustc <kjoshy12@gmail.com>
2017-02-26 12:10:41 -0800

    update frontend code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,46 @@
 import { Component, AfterViewInit } from '@angular/core';
+// import { FileUploader } from 'ng2-file-upload';
+import {UPLOAD_DIRECTIVES} from 'ng2-file-uploader/ng2-file-uploader';
 
 declare var $;
 
+const URL = 'http://localhost:5000/upload';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit{
-    title = 'app works!';
+    // public uploader:FileUploader = new FileUploader({url: URL});
+    // public hasBaseDropZoneOver:boolean = false;
+    filePath = '';
+    postId: number;
+    options: Object = {
+        url: 'http://localhost:5000/upload',
+        params: { 'post_id': this.postId }
+    };
+
+    handleUpload(data): void {
+        if (data && data.response) {
+            data = JSON.parse(data.response);
+            data = JSON.parse(data)
+            this.filePath = 'http://localhost:5000/file/'+data.filename;
+            setTimeout(() => {
+                this.activateAreaSelection()
+            }, 200);
+        }
+    }
 
     ngAfterViewInit() {
-        console.log($)
+    }
+
+    sendCoors(c) {
+        console.log(c)
+    }
+
+    activateAreaSelection() {
+        $('#input-image').Jcrop({
+            onSelect: this.sendCoors
+        })
     }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { IndexPageComponent } from './index-page/index-page.component';
+import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload'; 
+import {UPLOAD_DIRECTIVES} from 'ng2-file-uploader/ng2-file-uploader';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexPageComponent,
+    UPLOAD_DIRECTIVES
   ],
   imports: [
     BrowserModule,
