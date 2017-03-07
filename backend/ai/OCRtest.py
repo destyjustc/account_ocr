09ede3f5560e3b4b2699a19383ca3a3f331cb349
yong.zhang <yong.zhang@istuary.com>
2017-03-06 22:16:01 -0800

    Improved OCR

--- backend/ai/OCRtest.py
+++ backend/ai/OCRtest.py
@@ -6,50 +6,63 @@ import matplotlib.pyplot as plt
 import argparse
 
 
-def OCRTextLine(img, baseHeight=100, blurSize=5, threshold=180, lang='eng', boxes=False, showPlots=False):
+def OCRTextLine(img, baseHeight=100, blurSize=5, threshold=200, lang='eng', boxes=False, showPlots=False):
     height, width = img.shape[:2]
     scale = baseHeight/height
     newHeight = int(baseHeight)
     newWidth = int(scale*width)
     imgres = cv2.resize(img,(newWidth, newHeight), interpolation=cv2.INTER_LINEAR)
-    gray = cv2.cvtColor(imgres, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(blurSize,blurSize),0)
-    binary_output = np.zeros_like(blur)
-    binary_output[blur < threshold] = 1
-    img = Image.fromarray(np.uint8((binary_output)))
+    #gray = cv2.cvtColor(imgres, cv2.COLOR_RGB2YCrCb)[:,:,0]
+    #blur = cv2.GaussianBlur(gray,(blurSize,blurSize),0)
+    #binary_output = np.zeros_like(blur)
+    #binary_output[blur < threshold] = 1
+    img = Image.fromarray(np.uint8((imgres)))
     output = pytesseract.image_to_string(img, lang, boxes, config="-psm 7")
     if showPlots:
         plt.figure()
-        plt.subplot(141)
         plt.imshow(imgres)
-        plt.title('original')
-        plt.axis('off')
-        plt.subplot(142)
-        plt.imshow(gray, cmap='gray')
-        plt.title('gray')
-        plt.axis('off')
-        plt.subplot(143)
-        plt.imshow(blur, cmap='gray')
-        plt.title('blur')
-        plt.axis('off')
-        plt.subplot(144)
-        plt.imshow(binary_output, cmap='gray')
-        plt.title('binary')
-        plt.axis('off')
+        # plt.figure()
+        # plt.subplot(141)
+        # plt.imshow(imgres)
+        # plt.title('original')
+        # plt.axis('off')
+        # plt.subplot(142)
+        # plt.imshow(gray, cmap='gray')
+        # plt.title('gray')
+        # plt.axis('off')
+        # plt.subplot(143)
+        # plt.imshow(blur, cmap='gray')
+        # plt.title('blur')
+        # plt.axis('off')
+        # plt.subplot(144)
+        # plt.imshow(binary_output, cmap='gray')
+        # plt.title('binary')
+        # plt.axis('off')
     return output
 
 
-def histConstruct(img, axis=1, blurSize=9, threshold=180, showPlot=False):
+def histConstruct(img, axis=1, blurSize=9, threshold=220, showPlot=False):
     height, width = img.shape[:2]
     img = cv2.resize(img,(width, height), interpolation = cv2.INTER_NEAREST)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(blurSize, blurSize),0)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)[:, :, 0]
+    blur = cv2.GaussianBlur(gray,(blurSize, blurSize), 0)
     binary_output = np.zeros_like(blur)
     binary_output[blur < threshold] = 1
     hist = np.sum(binary_output, axis)
     if showPlot:
         plt.figure()
+        plt.subplot(2,2,1)
+        plt.imshow(img)
+        plt.subplot(2,2,2)
+        plt.imshow(gray, cmap='gray')
+        plt.subplot(2,2,3)
+        plt.imshow(blur, cmap='gray')
+        plt.subplot(2,2,4)
+        plt.imshow(binary_output, cmap='gray')
+        plt.show()
+        plt.figure()
         plt.plot(hist)
+        plt.show()
     return hist
 
 
@@ -71,12 +84,14 @@ def cropLines(img, lineloc, offset=4):
 
 def pipeline(filename, top_left = (0,0), bottom_right = None):
     img = cv2.imread(filename)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     if bottom_right is None:
         bottom_right = (img.shape[0], img.shape[1])
-    img = img[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]]
-    hist = histConstruct(img)
+    img = img[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1], :]
+    hist = histConstruct(img, showPlot=False)
     linesLocation = findTextLine(hist)
     listOfresults = list()
+    print(linesLocation)
     for lineloc in linesLocation:
         listOfresults.append(OCRTextLine(cropLines(img, lineloc)))
     return linesLocation, listOfresults
